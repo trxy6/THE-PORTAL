29d055b47128d7ae1824b3c3008039c94c424c1b
Portal Traveler <traveler@portal.local>
2026-07-10 22:50:19 -0600

    Rename audio asset to orbital-boot-sequence.wav to avoid URL spaces and trigger a production build update

--- src/components/PecosOnboarding.tsx
+++ src/components/PecosOnboarding.tsx
@@ -64,7 +64,7 @@ export default function PecosOnboarding({
         audio.play().catch(() => {});
       }
     } else {
-      audio = new Audio('/Orbital Boot Sequence.wav');
+      audio = new Audio('/orbital-boot-sequence.wav');
       audio.loop = true;
       audio.volume = 0.45;
       audio.play().catch(e => {
